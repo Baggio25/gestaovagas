61946e3f9007ac4f0a0d21e1c4259600e78f7ff9
rodrigo.baggio.si@gmail.com <Baggio25>
2024-01-26 16:48:41 -0300

    Creatind candidate table

--- src/main/java/com/baggio/gestaovagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/baggio/gestaovagas/modules/candidate/CandidateEntity.java
@@ -1,16 +1,29 @@
 package com.baggio.gestaovagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "candidate")
 public class CandidateEntity {
   
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
   private String name;
 
@@ -24,5 +37,13 @@ public class CandidateEntity {
   private String password;
   private String description;
   private String curriculum;
+
+  @CreationTimestamp
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+  
+  @UpdateTimestamp
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
 
 }
--- src/main/java/com/baggio/gestaovagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/baggio/gestaovagas/modules/candidate/controllers/CandidateController.java
@@ -10,7 +10,7 @@ import com.baggio.gestaovagas.modules.candidate.CandidateEntity;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/candidate")
+@RequestMapping("/candidates")
 public class CandidateController {
   
   @PostMapping
